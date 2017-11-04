374d897f67327d86e5aee15fbc7fc594fb7a623b
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-04 10:46:56 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,3 +2,10 @@ var playlist = {
   tchan: 'no hawai',
   spc: 'barata'
 }
+
+function updatePlaylist(playlist, artistName, songTitle){
+  playlist = {
+    [artistName]:songTitle;
+  }
+  return playlist;
+}
