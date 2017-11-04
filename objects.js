9683ce6d28bf459eb3a983a73fc686f143d67786
Julio Martelosso <martelossomartin1506.student@ifosupwavre.be>
2017-11-04 11:13:29 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -4,9 +4,11 @@ var playlist = {
 }
 
 function updatePlaylist(playlist, artistName, songTitle){
-  Object.assign(playlist,{
-    [artistName]:songTitle
-  });
+  // Object.assign(playlist,{
+  //   [artistName]:songTitle
+  // });
+
+  playlist.artistName = [songTitle];
   return playlist;
 }
 
